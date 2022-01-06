f465bdcd853e619b73a00390350e1cd32d6ed905
alelouis <me@alelouis.eu>
2022-01-06 16:32:19 +0100

    Adds system RT and common messages

--- src/messages.rs
+++ src/messages.rs
@@ -23,17 +23,33 @@ pub mod message {
     }
 }
 
-// Midi status, first byte
+// System Common Message   Status Byte      Number of Data Bytes
+// ---------------------   -----------      --------------------
+// MIDI Timing Code            F1                   1
+// Song Position Pointer       F2                   2
+// Song Select                 F3                   1
+// Tune Request                F6                  None
+
 #[derive(Debug)]
 pub enum Status {
-    NoteOff,
-    NoteOn,
-    PolyphonicKeyPressure,
-    ControlChange,
-    ProgramChange,
-    ChannelPressure,
-    PitchBend,
-    Unknown,
+    NoteOff,               // 8x
+    NoteOn,                // 9x
+    PolyphonicKeyPressure, // Ax
+    ControlChange,         // Bx
+    ProgramChange,         // Cx
+    ChannelPressure,       // Dx
+    PitchBend,             // Ex
+    MidiTimingCode,        // F1
+    SongPositionPointer,   // F2
+    SongSelect,            // F3
+    TuneRequest,           // F6
+    TimingClock,           // F8
+    StartSequence,         // FA
+    ContinueSequence,      // FB
+    StopSequence,          // FC
+    ActiveSensing,         // FE
+    SystemReset,           // FF
+    Unknown,               // ??
 }
 
 // Midi data, second and optional third bytes
@@ -55,6 +71,7 @@ pub enum Data {
     OmniModeOn,
     MonoModeOn,
     PolyModeOn,
+    Generic(u8),
     None,
 }
 
@@ -163,6 +180,74 @@ impl message::Raw {
                 stamp: self.stamp,
                 status: Status::PitchBend,
                 data: [Data::MSB(self.data[0]), Data::LSB(self.data[1])],
+            },
+            "f" => match &status_hex[1..] {
+                "1" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::MidiTimingCode,
+                    data: [Data::Generic(self.data[1]), Data::None],
+                },
+                "2" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::SongPositionPointer,
+                    data: [Data::Generic(self.data[1]), Data::Generic(self.data[2])],
+                },
+                "3" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::SongSelect,
+                    data: [Data::Generic(self.data[1]), Data::None],
+                },
+                "6" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::TuneRequest,
+                    data: [Data::None, Data::None],
+                },
+                "8" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::TimingClock,
+                    data: [Data::None, Data::None],
+                },
+                "a" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::StartSequence,
+                    data: [Data::None, Data::None],
+                },
+                "b" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::ContinueSequence,
+                    data: [Data::None, Data::None],
+                },
+                "c" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::StopSequence,
+                    data: [Data::None, Data::None],
+                },
+                "e" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::ActiveSensing,
+                    data: [Data::None, Data::None],
+                },
+                "f" => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::SystemReset,
+                    data: [Data::None, Data::None],
+                },
+                _ => message::Midi {
+                    channel: 16,
+                    stamp: self.stamp,
+                    status: Status::ControlChange,
+                    data: [Data::ResetAllControllers, Data::None],
+                },
             },
             _ => message::Midi {
                 channel: u8::from_str_radix(&status_hex[1..], 16).unwrap(),
