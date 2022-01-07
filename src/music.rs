9283806b290646f184025b2e85adbe68831509fb
alelouis <me@alelouis.eu>
2022-01-07 19:09:10 +0100

    Move voicelead to Chord method

--- src/music.rs
+++ src/music.rs
@@ -1,4 +1,5 @@
 use crate::messages::Data;
+use itertools::Itertools;
 use std::fmt;
 
 pub const KEYBOARD: [Letter; 12] = [
@@ -108,16 +109,64 @@ impl fmt::Display for Note {
 }
 
 impl Chord {
+    // Construct from Note vector
     pub fn new(notes: Vec<Note>) -> Self {
-        Chord { notes }
+        Self { notes }
     }
 
+    // Construct chord from vector str slices
     pub fn from_str(notes: Vec<&str>) -> Self {
-        Chord::new(
+        Self::new(
             notes
                 .iter()
                 .map(|note| Note::from_str(note).unwrap())
                 .collect(),
         )
     }
+
+    // Finds optimal minimum movement chord to target
+    pub fn voicelead_to(&self, target: &Self) -> Option<Self> {
+        let mut dist_vec: Vec<Vec<Vec<u8>>> = vec![];
+        let mut max: u32 = 100;
+        let mut voice_lead: Option<Chord> = None;
+        let chord_len: usize = 4;
+
+        // Computing distance vector between two chords
+        for note in &self.notes {
+            let mut note_vec: Vec<Vec<u8>> = vec![];
+            for other_note in &target.notes {
+                let mut octave_vec: Vec<u8> = vec![];
+
+                // Octave span should be equal to chord length
+                for octave in other_note.octave - 1..=other_note.octave + 2 {
+                    let swipe_note = Note::new(other_note.letter, octave);
+                    octave_vec.push(note.dist_to(&swipe_note));
+                }
+                note_vec.push(octave_vec);
+            }
+            dist_vec.push(note_vec);
+        }
+
+        // Finding minimal movement cost chord
+        for p in (0..chord_len).permutations(chord_len) {
+            for c in (0..chord_len).combinations_with_replacement(chord_len) {
+                let mut sum: u32 = 0;
+                for n in 0..chord_len {
+                    sum += dist_vec[n][p[n]][c[n]] as u32;
+                }
+                if sum < max {
+                    max = sum;
+                    let mut note_vec: Vec<Note> = vec![];
+                    for note in 0..chord_len {
+                        note_vec.push(Note::new(
+                            target.notes[p[note]].letter,
+                            target.notes[p[note]].octave + c[note] as u8 - 1,
+                        ));
+                    }
+                    voice_lead = Some(Chord::new(note_vec));
+                }
+            }
+        }
+        voice_lead
+        }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,3 @@
-use itertools::Itertools;
-use std::vec;
-
 use crate::messages;
 use crate::music;
 use crate::music::{Chord, Letter, Note};
@@ -19,6 +16,7 @@ fn from_key_number_to_note() {
     }
 }
 
+// Note creation from string
 #[test]
 fn note_from_str() {
     let a = Note::from_str("A0").unwrap();
@@ -30,6 +28,7 @@ fn note_from_str() {
     assert_eq!(bb.octave, 2);
 }
 
+// Distances in semitones between two Notes
 #[test]
 fn distance_between_notes() {
     let note_1 = Note {
@@ -59,6 +58,7 @@ fn distance_between_notes() {
     assert_eq!(note_4.dist_to(&note_5), 1);
 }
 
+// Chord creation from struct
 #[test]
 fn chord_from_struct() {
     let note_1 = Note {
@@ -77,6 +77,7 @@ fn chord_from_struct() {
     assert_eq!(chord.notes.len(), 3);
 }
 
+// Chord creation from string
 #[test]
 fn chord_from_str() {
     let chord = Chord::from_str(vec!["C0", "E1", "G2"]);
@@ -85,52 +86,13 @@ fn chord_from_str() {
     assert_eq!(chord.notes[2].letter, Letter::G);
 }
 
+// Chord optimal voice leading
 #[test]
 fn chord_transition() {
     let from = Chord::from_str(vec!["C4", "E4", "G4", "B4"]);
     let target = Chord::from_str(vec!["E4", "G4", "B4", "D5"]);
-    let mut dist_vec: Vec<Vec<Vec<u8>>> = vec![];
-
-    for note in &from.notes {
-        let mut note_vec: Vec<Vec<u8>> = vec![];
-        for other_note in &target.notes {
-            let mut octave_vec: Vec<u8> = vec![];
-            for octave in other_note.octave - 1..=other_note.octave + 2 {
-                let swipe_note = Note::new(other_note.letter, octave);
-                octave_vec.push(note.dist_to(&swipe_note));
-            }
-            note_vec.push(octave_vec);
-        }
-        dist_vec.push(note_vec);
-    }
-
-    println!("{:?}", dist_vec);
-
-    let mut max: u32 = 100;
-    let mut voice_lead: Option<Chord> = None;
-    let chord_len: usize = 4;
-
-    for p in (0..chord_len).permutations(chord_len) {
-        for c in (0..chord_len).combinations_with_replacement(chord_len) {
-            let mut sum: u32 = 0;
-            for n in 0..chord_len {
-                sum += dist_vec[n][p[n]][c[n]] as u32;
-            }
-            if sum < max {
-                max = sum;
-                let mut note_vec: Vec<Note> = vec![];
-                for note in 0..chord_len {
-                    note_vec.push(Note::new(
-                        target.notes[p[note]].letter,
-                        target.notes[p[note]].octave + c[note] as u8 - 1,
-                    ));
-                }
-                voice_lead = Some(Chord::new(note_vec));
-            }
-        }
-    }
-
-    
-    println!("{:?}", max);
-    println!("{:?}", voice_lead);
+    let voiceleaded = from.voicelead_to(&target).unwrap();
+    assert_eq!(voiceleaded.notes.len(), target.notes.len());
+    println!("{:?}", target);
+    println!("{:?}", voiceleaded);
 }
