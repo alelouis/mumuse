426db6c1187ecfa25f6dda53374d9a619f3c6b53
alelouis <me@alelouis.eu>
2022-01-14 22:44:49 +0100

    Add chord example

---
+++ examples/chord.rs
@@ -0,0 +1,15 @@
+extern crate mumuse;
+
+use mumuse::music::chord::Chord;
+use mumuse::music::note::Note;
+
+fn main() {
+    // From an str (Vector) directly
+    let chord_from_str= Chord::from_str(vec!["C0", "E1", "G2"]);
+    println!("From str vector : {}", chord_from_str);
+
+    // Same chord, but from a Vector Note
+    let notes = vec!["C0", "E1", "G2"].iter().map(|x| Note::from_str(x).unwrap()).collect();
+    let chord_from_notes = Chord::new(notes);
+    println!("From Note vector : {}", chord_from_notes)
+}
